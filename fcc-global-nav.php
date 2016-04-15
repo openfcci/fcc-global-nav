b8d6725e35483e9a777e9be99514b6c99561ca3d
ryan.veitch <ryan.r.veitch@gmail.com>
2016-04-15 15:26:54 -0500

    Rebased from master

--- fcc-global-nav.php
+++ fcc-global-nav.php
@@ -51,36 +51,28 @@ function remove_admin_bar_links() {
 	if ( ! is_admin() ) {
     global $wp_admin_bar;
 
-		/* Default Group (wp-admin-bar-root-default) */
+		/* Default Group: Left Side (wp-admin-bar-root-default) */
     $wp_admin_bar->remove_menu('wp-logo');          	// Remove the WordPress logo
-	    //$wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
-	    //$wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
-	    //$wp_admin_bar->remove_menu('documentation');    // Remove the WordPress documentation link
-	    //$wp_admin_bar->remove_menu('support-forums');   // Remove the support forums link
-	    //$wp_admin_bar->remove_menu('feedback');         // Remove the feedback link
-
+		$wp_admin_bar->remove_menu('my-sites');
     $wp_admin_bar->remove_menu('site-name');        // Remove the site name menu
 		$wp_admin_bar->remove_menu('customize');
-    $wp_admin_bar->remove_menu('view-site');        // Remove the view site link
-    $wp_admin_bar->remove_menu('updates');          // Remove the updates link
-    $wp_admin_bar->remove_menu('comments');         // Remove the comments link
-    $wp_admin_bar->remove_menu('new-content');      // Remove the content link
-    //$wp_admin_bar->remove_menu('w3tc');             // If you use w3 total cache remove the performance link
+		$wp_admin_bar->remove_menu('updates');          // Remove the updates link
+		$wp_admin_bar->remove_menu('comments');         // Remove the comments link
+		$wp_admin_bar->remove_menu('new-content');      // Remove the content link
+		$wp_admin_bar->remove_menu('stats');						// Remove Jetpack Stats
 
-		$wp_admin_bar->remove_menu('stats');
-
-    $wp_admin_bar->remove_menu('my-account');       // Remove the user details tab
+    /* Secondary Group: Right Side (wp-admin-bar-top-secondary)
+			HTML Order:   Search, My Account, Debug Bar
+			Screen Order: Debug, My Account, Search */
 		$wp_admin_bar->remove_menu('search');
-
-		$wp_admin_bar->remove_menu('my-sites');
-
+    $wp_admin_bar->remove_menu('my-account');       // Remove the user details tab
 
 	}
 }
 add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
 
 /*--------------------------------------------------------------
-# AV Logo Link
+# "AreaVoices" Logo Homepage Link
 --------------------------------------------------------------*/
 
 add_action( 'admin_bar_menu', 'add_av_logo_admin_bar_link', 1 );
@@ -158,12 +150,23 @@ function add_homepage_admin_bar_link( $wp_admin_bar ) {
 
 	if ( ! $user_id ) {
 		// Show 'Log In'
-		$args = array(
+		$wp_admin_bar->add_menu( array(
+			'id'        => 'av-login',
+			'parent'    => 'top-secondary',
+			'title'     => 'Log In',
+			'href'      => wp_login_url(),
+			'meta'      => array(
+				'class'     => '$class',
+			),
+		) );
+
+
+		/*$args = array(
 	    'id' => 'av-login',
 	    'title' => __( 'Log In' ),
 	    'href' => wp_login_url()
 	  );
-	  $wp_admin_bar->add_node($args);
+	  $wp_admin_bar->add_node($args);*/
 	} else {
 		// Show 'Dashboard'
 		$wp_admin_bar->add_menu( array(
